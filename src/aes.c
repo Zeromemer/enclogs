ccc4215f0c8090c35561cee57f1a16c33bf6928d
Zeromemer <zeromemer@users.noreply.github.com>
2022-05-22 02:13:26 +0300

    minor changes renamed  "length" variable to "in_length" int encryption and decryption functions for clarity modified a comment switched order of offseting the output buffer in encryption and decryption functions

--- src/aes.c
+++ src/aes.c
@@ -1,17 +1,17 @@
 #include "include/aes.h"
 
-// "inspired" by https://github.com/DaniloVlad/OpenSSL-AES
+// Sourced from https://github.com/DaniloVlad/OpenSSL-AES
 
-int aes256_encrypt(unsigned char *input, int length, unsigned char *output, unsigned char *key, unsigned char *iv) {
+int aes256_encrypt(unsigned char *input, int in_length, unsigned char *output, unsigned char *key, unsigned char *iv) {
 	EVP_CIPHER_CTX *ctx;
 	int result_len;
-	int len = length + (AES_BLOCK_SIZE - length % AES_BLOCK_SIZE);
+	int len = MAX_ENC_LENGTH(in_length);
 
 	//set up encryption context
 	ctx = EVP_CIPHER_CTX_new();
 	EVP_EncryptInit(ctx, EVP_aes_256_cbc(), key, iv);
 	//encrypt all the bytes up to but not including the last block
-	if(!EVP_EncryptUpdate(ctx, output, &len, input, length)) {
+	if(!EVP_EncryptUpdate(ctx, output, &len, input, in_length)) {
 		EVP_CIPHER_CTX_cleanup(ctx);
 		printf("EVP Error: couldn't update encryption with plain text!\n");
 		return 1;
@@ -19,7 +19,7 @@ int aes256_encrypt(unsigned char *input, int length, unsigned char *output, unsi
 	//update length with the amount of bytes written
 	result_len = len;
 	//EncryptFinal will cipher the last block + Padding
-	if(!EVP_EncryptFinal_ex(ctx, len + output, &len)) {
+	if(!EVP_EncryptFinal_ex(ctx, output + len, &len)) {
 		EVP_CIPHER_CTX_cleanup(ctx);
 		printf("EVP Error: couldn't finalize encryption!\n");
 		return 1;
@@ -31,7 +31,7 @@ int aes256_encrypt(unsigned char *input, int length, unsigned char *output, unsi
 	return result_len;
 }
 
-int aes256_decrypt(unsigned char *input, int length, unsigned char *output, unsigned char *key, unsigned char *iv) {
+int aes256_decrypt(unsigned char *input, int in_length, unsigned char *output, unsigned char *key, unsigned char *iv) {
 	EVP_CIPHER_CTX *ctx;
 	int result_len;
 	int len = 0;
@@ -39,13 +39,13 @@ int aes256_decrypt(unsigned char *input, int length, unsigned char *output, unsi
 	ctx = EVP_CIPHER_CTX_new();
 	EVP_DecryptInit(ctx, EVP_aes_256_cbc(), key, iv);
 	//same as above
-	if(!EVP_DecryptUpdate(ctx, output, &len, input, length)) {
+	if(!EVP_DecryptUpdate(ctx, output, &len, input, in_length)) {
 		EVP_CIPHER_CTX_cleanup(ctx);
 		printf("EVP Error: couldn't update decrypt with text!\n");
 		return 1;
 	}  
 	result_len = len;
-	if(!EVP_DecryptFinal_ex(ctx, len + output, &len)) {
+	if(!EVP_DecryptFinal_ex(ctx, output + len, &len)) {
 		EVP_CIPHER_CTX_cleanup(ctx);
 		printf("EVP Error: couldn't finalize decryption!\n");
 		return 1;
